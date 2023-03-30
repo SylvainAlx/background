b7302f52141a4e26d44e2bdbfb591e78b167275f
SylvainAlx <sylval49@gmail.com>
2023-03-30 16:08:24 +0200

    link h1

--- client/src/layouts/HeaderLayout.js
+++ client/src/layouts/HeaderLayout.js
@@ -1,11 +1,14 @@
 import "../assets/styles/Header.scss";
 import Nav from "../components/nav/Nav.js";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <header>
       <div className="title">
-        <h1>background</h1>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <h1>background</h1>
+        </Link>
         <em>worldbuilding pour créateurs d'histoires</em>
       </div>
       <Nav />
